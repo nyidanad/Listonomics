684c23fb497d51ec3f435c2725825a35ab119874
Nyíri Dániel <nyiridaniel3@gmail.com>
2026-02-10 21:12:32 +0100

    ci: finish up language picker modal

--- components/modals/settings/languageModal.tsx
+++ components/modals/settings/languageModal.tsx
@@ -1,8 +1,11 @@
-import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { Dispatch, SetStateAction, useState } from 'react'
+
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 import assets from '../../../data/assets.json'
 import settings from '../../../data/settings.json'
+import { Image } from 'expo-image'
 
 type settingsModalProps = {
   showModal: boolean
@@ -10,8 +13,21 @@ type settingsModalProps = {
   request?: () => Promise<void>
 }
 
+type LanguageKey = keyof typeof languageImages
+
+type Language = {
+  key: LanguageKey;
+  text: string;
+}
+
+const languageImages = {
+  en: require('../../../assets/images/flags/english-logo.png'),
+  de: require('../../../assets/images/flags/german-logo.png'),
+  hu: require('../../../assets/images/flags/hungary-logo.png'),
+}
+
 const languageModal = ({ showModal, setShowModal, request }: settingsModalProps) => {
-  const languages = assets.languages
+  const languages = assets.languages as Language[]
   const [activeLanguage, setAactiveLanguage] = useState(settings.language)
 
   const onSave = () => {}
@@ -38,12 +54,21 @@ const languageModal = ({ showModal, setShowModal, request }: settingsModalProps)
           {/* Languages */}
           <View>
             {languages.map((language, index) => {
+              const isActive = activeLanguage === language.key
+
               return (
-                <View key={index}>
-                  <Image width={50} height={50} source={{uri: language.image}} />
-                  <Text style={styles.language}>{language.text}</Text>
-                  {language.text === "English" && <Text style={styles.default}>Default</Text>}
-                </View>
+                <TouchableOpacity 
+                  key={index} 
+                  style={[styles.languageContainer, isActive && {backgroundColor: '#FFF'}]} 
+                  onPress={() => setAactiveLanguage(language.key)}
+                >
+                  <View style={styles.languageWrapper}>
+                    <Image style={styles.image} source={languageImages[language.key]} />
+                    <Text style={styles.language}>{language.text}</Text>
+                    {language.text === "English" && <Text style={styles.default}>Default</Text>}
+                  </View>
+                  {isActive && <Ionicons name='checkmark-outline' style={styles.checkmark} />}
+                </TouchableOpacity>
               )
             })}
           </View>
@@ -68,42 +93,77 @@ export default languageModal
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0.3)',
   },
   content: {
     backgroundColor: '#FAFAFA',
-    paddingHorizontal: 20,
     paddingVertical: 15,
     borderRadius: 15,
     elevation: 5,
     width: '90%',
     height: 'auto',
+    marginBottom: 20,
   },
   title: {
     color: '#363636',
     fontSize: 18,
     fontWeight: 'bold',
+    paddingHorizontal: 20,
     marginBottom: 15,
   },
   message: {
     color: '#989CA9',
     fontSize: 14,
-    marginBottom: 30,
+    paddingHorizontal: 20,
+    marginBottom: 17,
   },
-  flag: {
+  languageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
+    marginVertical: 3,
+  },
+  languageWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 35,
+    height: 35,
+    borderRadius: 999,
+    marginLeft: 20,
+    marginRight: 15,
   },
   language: {
-
+    color: '#363636',
+    fontSize: 16,
   },
   default: {
-
+    color: '#989CA9',
+    fontSize: 10,
+    borderRadius: 999,
+    backgroundColor: '#F1F1F1',
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    marginLeft: 20,
+  },
+  checkmark: {
+    color: '#2ECC71',
+    fontSize: 18,
+    borderRadius: 999,
+    padding: 1,
+    backgroundColor: 'rgba(46, 204, 113, 0.25)',
+    marginRight: 20,
   },
   buttonWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    paddingHorizontal: 20,
+    marginTop: 12,
   },
   button: {
     justifyContent: 'center',
